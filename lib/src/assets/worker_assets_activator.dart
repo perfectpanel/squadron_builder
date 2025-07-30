465775ea902b83dc44d7489d7848b36b991cc830
Maxim Belyakov <maximbell@perfectpanel.com>
2025-07-30 09:30:03 +0300

    js-app-config: changed import order

--- lib/src/assets/worker_assets_activator.dart
+++ lib/src/assets/worker_assets_activator.dart
@@ -41,15 +41,18 @@ extension ActivatorExt on WorkerAssets {
   void generateWebCode(BuildStepCodeEvent codeEvent) {
     final output = _webOutput;
     if (output != null) {
-      codeEvent.importSquadron(output, _squadronAlias);
-      codeEvent.importDartCore(output, _dartCoreAlias);
-      codeEvent.import(output, codeEvent.buildStep.inputId);
       codeEvent.add(
         output,
         "import 'dart:js_util';",
       );
-      codeEvent.add(output,
-          "import 'package:user_app/core/utils/js/js_object_converter.dart';", );
+      codeEvent.add(
+        output,
+        "import 'package:user_app/core/utils/js/js_object_converter.dart';",
+      );
+      codeEvent.importSquadron(output, _squadronAlias);
+      codeEvent.importDartCore(output, _dartCoreAlias);
+
+      codeEvent.import(output, codeEvent.buildStep.inputId);
 
       codeEvent.addWebEntryPoint(
         output,
